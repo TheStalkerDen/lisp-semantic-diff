276a639ad2ac4d769f7c14893eefb993638c0419
Den Ermol <denisermolenko17@gmail.com>
2021-01-16 14:02:16 +0200

    Added lexem class

--- DiffBackend/src/lexer.lisp
+++ DiffBackend/src/lexer.lisp
@@ -1,9 +1,36 @@
 (uiop:define-package :diff-backend/lexer
     (:nicknames :lexer)
   (:use :cl)
-  (:export #:lexer))
+  (:export #:lexer
+           #:lexem))
 
 (in-package :diff-backend/lexer)
 
-(defun lexer ()
+(declaim (optimize safety))
+
+(defclass lexem ()
+  ((line
+    :accessor lexem-line
+    :initarg :line
+    :type integer)
+   (column
+    :accessor lexem-column
+    :initarg :column
+    :type integer)
+   (type
+    :accessor lexem-type
+    :initarg :type
+    :type symbol)
+   (string
+    :accessor lexem-string
+    :initarg :string
+    :type string))
+  (:documentation "Lexem info"))
+
+(defmethod print-object ((lex lexem) stream)
+  (with-slots (line column string) lex
+    (print-unreadable-object (lex stream)
+      (format stream "Lexem: ~S (~d:~d)" string line column))))
+
+(defun lexer (file-str)
   nil)
