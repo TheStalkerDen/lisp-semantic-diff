cf17d4acddfb3027a46b903cf3c538debad3e2a1
Den Ermol <denisermolenko17@gmail.com>
2021-01-25 22:32:32 +0200

    Fixed defclass*

--- DiffBackend/src/utils.lisp
+++ DiffBackend/src/utils.lisp
@@ -15,15 +15,16 @@
                          slot-specs)))
     `(progn
        (defclass ,name (,@superclasses)
-         ,(append 
-           (mapcar (lambda (spec)
-                     (let ((export-pos (position :export spec)))
-                       (if export-pos
-                           (append (subseq spec 0 export-pos)
-                                   (subseq spec (+ 2 export-pos)))
-                           spec)))
-                   slot-specs)
-           (when class-option (list class-option))))
+         ,@ (remove nil
+                    (list (mapcar
+                           (lambda (spec)
+                             (let ((export-pos (position :export spec)))
+                               (if export-pos
+                                   (append (subseq spec 0 export-pos)
+                                           (subseq spec (+ 2 export-pos)))
+                                   spec)))
+                           slot-specs)
+                          (when class-option class-option))))
        ,@(mapcar (lambda (name) `(export ',name))
                  exports)
        (export ',name))))
