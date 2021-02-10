b32dc9c9d3e0cb472793f461924560342fec0965
Den Ermol <denisermolenko17@gmail.com>
2021-02-10 22:14:57 +0200

    Improved deep-equal

--- DiffBackend/tests/test-engines.lisp
+++ DiffBackend/tests/test-engines.lisp
@@ -12,6 +12,8 @@
 
 (in-package :diff-backend/tests/test-engines)
 
+(declaim (optimize (debug 3)))
+
 (defmacro def-lexer-test (name str lexems-list)
   `(deftest ,name
      (let ((res (lexer ,str)))
@@ -39,18 +41,35 @@
 (defmacro def-ast-test (name str obj-tree)
   `(deftest ,name
      (let ((ast-gen-res (abstract-sem-tree-gen (parser (lexer ,str)))))
-       (break)
        (unless (deep-equal ast-gen-res ,obj-tree)
          (fail "AST-gen test failed")))))
 
 (defun deep-equal (obj1 obj2)
-  (when (eq (type-of obj1) (type-of obj2))
-    (typecase obj1 
-      (standard-object
-       (loop :for slot :in (closer-mop:class-slots (class-of obj1))
-          :for slot-name = (closer-mop:slot-definition-name slot)
-          :always (or (and (null (slot-boundp obj1 slot-name))
-                           (null (slot-boundp obj2 slot-name)))
-                      (deep-equal (slot-value obj1 slot-name)
-                                  (slot-value obj2 slot-name)))))
-      (t (equalp obj1 obj2)))))
+  (if (equal (type-of obj1) (type-of obj2))
+      (if (typecase obj1 
+                (standard-object
+                 (loop :for slot :in (closer-mop:class-slots (class-of obj1))
+                    :for slot-name = (closer-mop:slot-definition-name slot)
+                    :always (or (and (null (slot-boundp obj1 slot-name))
+                                     (null (slot-boundp obj2 slot-name)))
+                                (if (deep-equal (slot-value obj1 slot-name)
+                                                (slot-value obj2 slot-name))
+                                    t
+                                    (progn (print obj1)
+                                           (print obj2)
+                                           nil)))))
+                (list (every #'deep-equal obj1 obj2))
+                (t (unless (equalp obj1 obj2)
+                     (print obj1)
+                     (print obj2)
+                     (return-from deep-equal nil))
+                   t))
+          t
+          (progn
+            (print obj1)
+            (print obj2)
+            nil))
+    (progn (print "Not equal types")
+           (format t "Type: ~A obj1: ~A ~%" (type-of obj1) obj1)
+           (format t "Type: ~A obj2: ~A ~%" (type-of obj2) obj2)
+           nil)))
