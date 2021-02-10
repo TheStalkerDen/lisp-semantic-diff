c84484a5dbd7252dc6015f39e1f1aef5b657077f
Den Ermol <denisermolenko17@gmail.com>
2021-02-10 22:04:48 +0200

    Added defun.1 unit-test to abstract-sem-tree-generator

--- DiffBackend/tests/abstract-sem-tree-generator.lisp
+++ DiffBackend/tests/abstract-sem-tree-generator.lisp
@@ -1,5 +1,40 @@
 (uiop:define-package :diff-backend/tests/abstract-sem-tree-generator
     (:use :cl
-          :rove))
+          :diff-backend/nodes
+          :diff-backend/lexer
+          :diff-backend/tests/test-engines))
 
 (in-package :diff-backend/tests/abstract-sem-tree-generator)
+
+(def-ast-test defun.1
+    "(defun a () 1)"
+  (list (make-instance
+         'defun-node
+         :func-name (make-instance
+                     'lexem-wrapper-node
+                     :lexem-info (make-lexem
+                                  "a"
+                                  1
+                                  8
+                                  :symbol))
+         :keyword-lexem (make-instance
+                         'lexem-wrapper-node
+                         :lexem-info (make-lexem
+                                      "defun"
+                                      1
+                                      2
+                                      :symbol))
+         :parenthesis-info `((:lparen-coord 1 1)
+                             (:rparen-coord 1 14))
+         :parameters-list (make-instance
+                           'list-node
+                           :parenthesis-info `((:lparen-coord 1 10)
+                                               (:rparen-coord 1 11))
+                           :elements ())
+         :body-forms `(,(make-instance
+                         'lexem-wrapper-node
+                         :lexem-info (make-lexem
+                                      "1"
+                                      1
+                                      13
+                                      :integer))))))
