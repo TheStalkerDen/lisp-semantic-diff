f33b2827f3728f40f26d02a2224ef51bee17dbea
Den Ermol <denisermolenko17@gmail.com>
2021-02-11 21:28:35 +0200

    Added funcall.1 and funcall.2 unit-tests to abstract-sem-tree-generator

--- DiffBackend/tests/abstract-sem-tree-generator.lisp
+++ DiffBackend/tests/abstract-sem-tree-generator.lisp
@@ -6,35 +6,75 @@
 
 (in-package :diff-backend/tests/abstract-sem-tree-generator)
 
+(def-ast-test funcall.1
+    "(fun)"
+  (list
+   (make-instance
+    'function-call-node
+    :func-lexem (make-instance
+                 'lexem-wrapper-node
+                 :lexem-info (make-lexem
+                              "fun"
+                              1
+                              2
+                              :symbol))
+    :parenthesis-info `((:lparen-coord 1 1)
+                        (:rparen-coord 1 5))
+    :func-arg-forms ())))
+
+(def-ast-test funcall.2
+    "(fun 1)"
+  (list
+   (make-instance
+    'function-call-node
+    :func-lexem (make-instance
+                 'lexem-wrapper-node
+                 :lexem-info (make-lexem
+                              "fun"
+                              1
+                              2
+                              :symbol))
+    :parenthesis-info `((:lparen-coord 1 1)
+                        (:rparen-coord 1 7))
+    :func-arg-forms `(,(make-instance
+                        'lexem-wrapper-node
+                        :lexem-info
+                        (make-lexem
+                         "1"
+                         1
+                         6
+                         :integer))))))
+
 (def-ast-test defun.1
     "(defun a () 1)"
-  (list (make-instance
-         'defun-node
-         :func-name (make-instance
-                     'lexem-wrapper-node
-                     :lexem-info (make-lexem
-                                  "a"
-                                  1
-                                  8
-                                  :symbol))
-         :keyword-lexem (make-instance
-                         'lexem-wrapper-node
-                         :lexem-info (make-lexem
-                                      "defun"
-                                      1
-                                      2
-                                      :symbol))
-         :parenthesis-info `((:lparen-coord 1 1)
-                             (:rparen-coord 1 14))
-         :parameters-list (make-instance
-                           'list-node
-                           :parenthesis-info `((:lparen-coord 1 10)
-                                               (:rparen-coord 1 11))
-                           :elements ())
-         :body-forms `(,(make-instance
-                         'lexem-wrapper-node
-                         :lexem-info (make-lexem
-                                      "1"
-                                      1
-                                      13
-                                      :integer))))))
+  (list
+   (make-instance
+    'defun-node
+    :func-name (make-instance
+                'lexem-wrapper-node
+                :lexem-info (make-lexem
+                             "a"
+                             1
+                             8
+                             :symbol))
+    :keyword-lexem (make-instance
+                    'lexem-wrapper-node
+                    :lexem-info (make-lexem
+                                 "defun"
+                                 1
+                                 2
+                                 :symbol))
+    :parenthesis-info `((:lparen-coord 1 1)
+                        (:rparen-coord 1 14))
+    :parameters-list (make-instance
+                      'list-node
+                      :parenthesis-info `((:lparen-coord 1 10)
+                                          (:rparen-coord 1 11))
+                      :elements ())
+    :body-forms `(,(make-instance
+                    'lexem-wrapper-node
+                    :lexem-info (make-lexem
+                                 "1"
+                                 1
+                                 13
+                                 :integer))))))
