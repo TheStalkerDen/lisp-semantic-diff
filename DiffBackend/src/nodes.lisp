40964abf1b0cb2debdc381a01c90961401a6ad43
Den Ermol <denisermolenko17@gmail.com>
2021-02-13 22:01:26 +0200

    Added no-whitespace-length-mixin to nodes

--- DiffBackend/src/nodes.lisp
+++ DiffBackend/src/nodes.lisp
@@ -11,7 +11,7 @@
                        (&rest slot-specs)
                        &optional class-option)
   `(progn
-     (defclass* ,name (,@(nconc superclasses '(diff-status-mixin)))
+     (defclass* ,name (,@(nconc superclasses '(diff-status-mixin no-whitespace-length-mixin)))
        ,slot-specs
        ,(when class-option
           class-option))))
@@ -29,9 +29,31 @@
   ((keyword-lexem :accessor keyword-lexem
                   :initarg :keyword-lexem)))
 
+(defclass* no-whitespace-length-mixin ()
+  ((no-whitespace-length :accessor no-whitespace-length
+                         :initarg :no-whitespace-length)))
+
+(defgeneric calculate-no-whitespace-length (obj)
+  (:method (obj)
+    (print "No no")))
+
+(defmethod calculate-no-whitespace-length :around ((obj no-whitespace-length-mixin))
+  (if (slot-boundp obj 'no-whitespace-length)
+      (slot-value obj 'no-whitespace-length)
+      (call-next-method)))
+
+(defmethod initialize-instance :after ((obj no-whitespace-length-mixin) &key)
+  (with-slots (no-whitespace-length) obj
+    (unless (slot-boundp obj 'no-whitespace-length)
+      (setf no-whitespace-length (calculate-no-whitespace-length obj)))))
+
 (define-node lexem-wrapper-node ()
   ((lexem-info :accessor lexem-info
                :initarg :lexem-info)))
+
+(defmethod calculate-no-whitespace-length ((obj lexem-wrapper-node))
+  (with-slots (lexem-info) obj
+    (length (lexem-string lexem-info))))
 
 (defmethod print-object ((obj lexem-wrapper-node) stream)
   (with-slots (lexem-info) obj
@@ -48,11 +70,22 @@
    (body-forms :accessor body-forms
                :initarg :body-forms)))
 
+(defmethod calculate-no-whitespace-length ((obj defun-node))
+  (with-slots (function-name parameters-list body-forms) obj
+      (+ (calculate-no-whitespace-length function-name)
+         (calculate-no-whitespace-length parameters-list)
+         (calculate-no-whitespace-length body-forms))))
+
 (define-node function-call-node (parenthesis-mixin)
   ((func-lexem :accessor func-lexem
                :initarg :func-lexem)
    (func-arg-forms :accessor func-arg-forms
                    :initarg :func-arg-forms)))
+
+(defmethod calculate-no-whitespace-length ((obj function-call-node))
+  (with-slots (func-lexem func-arg-forms) obj 
+    (+ (calculate-no-whitespace-length func-lexem)
+       (calculate-no-whitespace-length func-arg-forms))))
 
 (defmethod print-object ((obj function-call-node) stream)
   (with-slots (func-lexem) obj
@@ -61,6 +94,14 @@
 
 (define-node list-node (parenthesis-mixin)
   ((elements :accessor elements
-              :initarg :elements)))
+             :initarg :elements)))
 
+(defmethod calculate-no-whitespace-length ((obj list-node))
+  (with-slots (elements) obj
+    (loop :for el :in elements
+         :sum (calculate-no-whitespace-length el))))
+
+(defmethod calculate-no-whitespace-length ((obj list))
+  (loop :for el :in obj
+       :sum (calculate-no-whitespace-length el)))
 
