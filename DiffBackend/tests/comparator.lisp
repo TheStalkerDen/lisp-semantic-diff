187253256a514041d7dd41e5aa6d67f5e459055b
Den Ermol <denisermolenko17@gmail.com>
2021-02-18 22:00:10 +0200

    Added mod-defuns.1 mixed.1 mixed.2 unit-tests to comparator

--- DiffBackend/tests/comparator.lisp
+++ DiffBackend/tests/comparator.lisp
@@ -35,3 +35,41 @@
   `((:defuns
         (:new
          ("a")))))
+
+(def-simple-classifier-test mod-defuns.1
+    "(defun a () 1)"
+  "(defun a () 4)"
+  `((:defuns
+        (:modified
+         ("a"))))
+  `((:defuns
+        (:modified
+         ("a")))))
+
+(def-simple-classifier-test mixed.1
+    "(defun a () 1)
+     (defun k () 9)"
+  "(defun a () 4)"
+  `((:defuns
+        (:modified
+         ("a"))
+        (:deleted
+         ("k"))))
+  `((:defuns
+        (:modified
+         ("a")))))
+
+(def-simple-classifier-test mixed.2
+    "(defun a (a) 1)
+     (defun k () 9)"
+  "(defun a (a) 4)"
+  `((:defuns
+        (:modified
+         ("a"))
+        (:deleted
+         ("k"))))
+  `((:defuns
+        (:modified
+         ("a")))))
+
+
