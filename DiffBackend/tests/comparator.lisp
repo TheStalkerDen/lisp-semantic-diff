05ed74c96e39ac2c1d598725e981ff4bf0c85004
Den Ermol <denisermolenko17@gmail.com>
2021-02-19 21:00:18 +0200

    Added cmp.test.1 unit-test to comparator

--- DiffBackend/tests/comparator.lisp
+++ DiffBackend/tests/comparator.lisp
@@ -1,5 +1,7 @@
 (uiop:define-package :diff-backend/tests/comparator
     (:use :cl
+     :diff-backend/lexer
+          :diff-backend/nodes
           :diff-backend/tests/test-engines))
 
 (in-package :diff-backend/tests/comparator)
@@ -81,5 +83,74 @@
   `((:defuns
         (:modified
          ("a")))))
+
+;;;TODO simplify
+(def-comparator-test cmp.test.1
+  "(defun a () 1)"
+  "(defun a () 2)"
+  (list
+   (make-instance
+    'defun-node
+    :func-name (make-instance
+                'lexem-wrapper-node
+                :lexem-info (make-lexem
+                             "a"
+                             1
+                             8
+                             :symbol))
+    :keyword-lexem (make-instance
+                    'lexem-wrapper-node
+                    :lexem-info (make-lexem
+                                 "defun"
+                                 1
+                                 2
+                                 :symbol))
+    :parenthesis-info `((:lparen-coord 1 1)
+                        (:rparen-coord 1 14))
+    :parameters-list (make-instance
+                      'list-node
+                      :parenthesis-info `((:lparen-coord 1 10)
+                                          (:rparen-coord 1 11))
+                      :elements ())
+    :body-forms `(,(make-instance
+                    'lexem-wrapper-node
+                    :diff-status :deleted
+                    :lexem-info (make-lexem
+                                 "1"
+                                 1
+                                 13
+                                 :integer)))))
+  (list
+   (make-instance
+    'defun-node
+    :func-name (make-instance
+                'lexem-wrapper-node
+                :lexem-info (make-lexem
+                             "a"
+                             1
+                             8
+                             :symbol))
+    :keyword-lexem (make-instance
+                    'lexem-wrapper-node
+                    :lexem-info (make-lexem
+                                 "defun"
+                                 1
+                                 2
+                                 :symbol))
+    :parenthesis-info `((:lparen-coord 1 1)
+                        (:rparen-coord 1 14))
+    :parameters-list (make-instance
+                      'list-node
+                      :parenthesis-info `((:lparen-coord 1 10)
+                                          (:rparen-coord 1 11))
+                      :elements ())
+    :body-forms `(,(make-instance
+                    'lexem-wrapper-node
+                    :diff-status :new
+                    :lexem-info (make-lexem
+                                 "2"
+                                 1
+                                 13
+                                 :integer))))))
 
 
