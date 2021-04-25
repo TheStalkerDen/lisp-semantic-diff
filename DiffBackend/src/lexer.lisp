67d2ac3694d9dd5529372ac07415a516d6f69d7c
Den Ermol <denisermolenko17@gmail.com>
2021-04-25 14:23:26 +0300

    Modified src/lexer.lisp : added string and comment support

--- DiffBackend/src/lexer.lisp
+++ DiffBackend/src/lexer.lisp
@@ -95,94 +95,146 @@
 (defun lexer (file-str)
   (let ((stream (make-string-input-stream file-str))
         (lexems)
+        (comments-table (make-hash-table))
+        (lex-errors)
         (line 1)
         (cur-lexem-column 1)
+        (cur-lexem-line 1)
         (column 1)
         (lexem-l))
     (prog
         ((cur-char (read-char stream nil)))
      COMMON
-     (setf cur-lexem-column column)
-     (cond
-       ((digit? cur-char) (go INTEGER))
-       ((or (alpha? cur-char)
-            (is-special-symbol? cur-char))
-        (go SYMBOL))
-       ((ch= cur-char #\( ) (go OUT_LEFT_PARENT))
-       ((ch= cur-char #\)) (go OUT_RIGHT_PARENT))
-       ((ch= cur-char #\') (go QUOTE))
-       ((is-whitespace? cur-char) (go WHITESPACE))
-       ((null cur-char) (go END))
-       (t (error "Incorrect char ~s~%" cur-char)))
+       (setf cur-lexem-column column)
+       (setf cur-lexem-line line)
+       (cond
+         ((digit? cur-char) (go INTEGER))
+         ((or (alpha? cur-char)
+              (is-special-symbol? cur-char))
+          (go SYMBOL))
+         ((ch= cur-char #\( ) (go OUT_LEFT_PARENT))
+         ((ch= cur-char #\)) (go OUT_RIGHT_PARENT))
+         ((ch= cur-char #\') (go QUOTE))
+         ((ch= cur-char #\") (go STRING))
+         ((ch= cur-char #\;) (go COMMENT))
+         ((is-whitespace? cur-char) (go WHITESPACE))
+         ((null cur-char) (go END))
+         (t (error "Incorrect char ~s~%" cur-char)))
      INTEGER
-     (incf column)
-     (push cur-char lexem-l)
-     (setf cur-char (read-char stream nil))
-     (cond
-       ((digit? cur-char)
-        (go INTEGER))
-       ((or (alpha? cur-char)
-            (is-special-symbol? cur-char))
-        (go SYMBOL))
-       (t (go OUT_INTEGER)))
+       (incf column)
+       (push cur-char lexem-l)
+       (setf cur-char (read-char stream nil))
+       (cond
+         ((digit? cur-char)
+          (go INTEGER))
+         ((or (alpha? cur-char)
+              (is-special-symbol? cur-char))
+          (go SYMBOL))
+         (t (go OUT_INTEGER)))
      OUT_INTEGER
-     (push (make-lexem (coerce (reverse lexem-l) 'string)
-                       line
-                       cur-lexem-column
-                       :integer)
-           lexems)
-     (setf lexem-l nil)
-     (go COMMON)
+       (push (make-lexem (coerce (reverse lexem-l) 'string)
+                         cur-lexem-line
+                         cur-lexem-column
+                         :integer)
+             lexems)
+       (setf lexem-l nil)
+       (go COMMON)
      SYMBOL
-     (incf column)
-     (push cur-char lexem-l)
-     (setf cur-char (read-char stream nil))
-     (cond ((or (digit-alpha? cur-char)
-                (is-special-symbol? cur-char))
-            (go SYMBOL))
-           (t (go OUT_SYMBOL)))
+       (incf column)
+       (push cur-char lexem-l)
+       (setf cur-char (read-char stream nil))
+       (cond ((or (digit-alpha? cur-char)
+                  (is-special-symbol? cur-char))
+              (go SYMBOL))
+             (t (go OUT_SYMBOL)))
      OUT_SYMBOL
-     (push (make-lexem (coerce (reverse lexem-l) 'string)
-                       line
-                       cur-lexem-column
-                       :symbol)
-           lexems)
-     (setf lexem-l nil)
-     (go COMMON)
+       (push (make-lexem (coerce (reverse lexem-l) 'string)
+                         line
+                         cur-lexem-column
+                         :symbol)
+             lexems)
+       (setf lexem-l nil)
+       (go COMMON)
      OUT_LEFT_PARENT
-     (push (make-lexem "("
-                       line
-                       cur-lexem-column
-                       :left-parent)
-           lexems)
-     (setf cur-char (read-char stream nil))
-     (incf column)
-     (go COMMON)
+       (push (make-lexem "("
+                         line
+                         cur-lexem-column
+                         :left-parent)
+             lexems)
+       (setf cur-char (read-char stream nil))
+       (incf column)
+       (go COMMON)
      OUT_RIGHT_PARENT
-     (push (make-lexem ")"
-                       line
-                       cur-lexem-column
-                       :right-parent)
-           lexems)
-     (setf cur-char (read-char stream nil))
-     (incf column)
-     (go COMMON)
+       (push (make-lexem ")"
+                         line
+                         cur-lexem-column
+                         :right-parent)
+             lexems)
+       (setf cur-char (read-char stream nil))
+       (incf column)
+       (go COMMON)
      WHITESPACE
-     (when (ch= cur-char #\Newline)
-       (setf column 0)
-       (incf line))
-     (incf column)
-     (setf cur-char (read-char stream nil))
-     (cond ((is-whitespace? cur-char) (go WHITESPACE))
-           (t (go COMMON)))
+       (when (ch= cur-char #\Newline)
+         (setf column 0)
+         (incf line))
+       (incf column)
+       (setf cur-char (read-char stream nil))
+       (cond ((is-whitespace? cur-char) (go WHITESPACE))
+             (t (go COMMON)))
      QUOTE
-     (push (make-lexem "'"
-                       line
-                       cur-lexem-column
-                       :quote)
-           lexems)
-     (incf column)
-     (setf cur-char (read-char stream nil))
-     (go COMMON)
+       (push (make-lexem "'"
+                         line
+                         cur-lexem-column
+                         :quote)
+             lexems)
+       (incf column)
+       (setf cur-char (read-char stream nil))
+       (go COMMON)
+     STRING
+       (when (ch= cur-char #\Newline)
+         (setf column 0)
+         (incf line))
+       (incf column)
+       (push cur-char lexem-l)
+       (setf cur-char (read-char stream nil))
+       (cond ((ch= cur-char #\\)
+              (go STRING_ESCAPE_SYMBOL))
+             ((ch= cur-char #\")
+              (push cur-char lexem-l)
+              (go OUT_STRING))
+             (t (go STRING)))
+     STRING_ESCAPE_SYMBOL
+       (incf column)
+       (push cur-char lexem-l)
+       (setf cur-char (read-char stream nil))
+       (go STRING)
+     OUT_STRING
+       (push (make-lexem (coerce (reverse lexem-l) 'string)
+                         cur-lexem-line
+                         cur-lexem-column
+                         :string)
+             lexems)
+       (setf lexem-l nil)
+       (setf cur-char (read-char stream nil))
+       (incf column)
+       (go COMMON)
+     COMMENT
+       (incf column)
+       (push cur-char lexem-l)
+       (setf cur-char (read-char stream nil))
+       (cond ((or (ch= cur-char #\Newline)
+                  (ch= cur-char #\Return)
+                  (null cur-char))
+              (go COMMENT_OUT))
+             (t (go COMMENT)))
+     COMMENT_OUT
+       (setf (gethash cur-lexem-line comments-table)
+             `(:comment ,(coerce (reverse lexem-l) 'string)
+               :column ,cur-lexem-column))
+       (setf lexem-l nil)
+       (go COMMON)
      END
-     (return (reverse lexems)))))
+       (return))
+    (values (reverse lexems)
+            comments-table
+            (reverse lex-errors))))
