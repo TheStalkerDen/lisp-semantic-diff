507c898313d7fa1816ba8d343cb9a0f3aa2b0b37
Den Ermol <denisermolenko17@gmail.com>
2021-04-26 23:13:17 +0300

    Added lex-errors support to main

--- DiffBackend/src/main.lisp
+++ DiffBackend/src/main.lisp
@@ -15,6 +15,10 @@
 
 (defparameter *comment-table-1* nil)
 (defparameter *comment-table-2* nil)
+(defparameter *lex-errors-msgs-1* nil)
+(defparameter *lex-errors-msgs-2* nil)
+(defparameter *lexems-1* nil)
+(defparameter *lexems-2* nil)
 
 (defun main ()
   (let ((cmd-args (uiop:command-line-arguments)))
@@ -25,7 +29,11 @@
 (defun differ-v01 (file1 file2)
   (init-stats)
   (let (*comment-table-1*
-        *comment-table-2*)
+        *comment-table-2*
+        *lex-errors-msgs-1*
+        *lex-errors-msgs-2*
+        *lexems-1*
+        *lexems-2*)
     (multiple-value-bind (res1 res2)
         (compare-two-files file1 file2)
       (generate-json-outputs res1 res2))))
@@ -33,7 +41,11 @@
 (defun str-differ-v01 (str1 str2)
   (init-stats)
   (let (*comment-table-1*
-        *comment-table-2*)
+        *comment-table-2*
+        *lex-errors-msgs-1*
+        *lex-errors-msgs-2*
+        *lexems-1*
+        *lexems-2*)
     (multiple-value-bind (res1 res2)
         (compare-two-str str1 str2)
       (generate-json-outputs res1 res2))))
@@ -47,29 +59,63 @@
     (with-open-file (stream "comments2.json" :direction :output
                                              :if-exists :supersede)
       (get-json-comments *comment-table-2* stream)))
-  (with-open-file (stream "res1.json" :direction :output
-                                      :if-exists :supersede)
-    (get-json-res res1 stream))
-  (with-open-file (stream "res2.json" :direction :output
-                                      :if-exists :supersede)
-    (get-json-res res2 stream))
-  (with-open-file (stream "stats.json" :direction :output
-                                       :if-exists :supersede)
-    (get-stats-res stream)))
+  (when *lex-errors-msgs-1*
+    (with-open-file (stream "lexems1.json" :direction :output
+                                          :if-exists :supersede)
+      (get-lexems-json *lexems-1* stream))
+    (with-open-file (stream "lexer-errors-msgs1.json" :direction :output
+                                                     :if-exists :supersede)
+      (get-lexer-errors-msgs-json *lex-errors-msgs-1* stream)))
+  (when *lex-errors-msgs-2*
+    (with-open-file (stream "lexems2.json" :direction :output
+                                          :if-exists :supersede)
+      (get-lexems-json *lexems-2* stream))
+    (with-open-file (stream "lexer-errors-msgs2.json" :direction :output
+                                                     :if-exists :supersede)
+      (get-lexer-errors-msgs-json *lex-errors-msgs-2* stream)))
+  (when (and res1 res2)
+    (with-open-file (stream "res1.json" :direction :output
+                                        :if-exists :supersede)
+      (get-json-res res1 stream))
+    (with-open-file (stream "res2.json" :direction :output
+                                        :if-exists :supersede)
+      (get-json-res res2 stream))
+    (with-open-file (stream "stats.json" :direction :output
+                                         :if-exists :supersede)
+      (get-stats-res stream))))
 
 (defun simple-differ-str (str1 str2 &optional (out1 t ) (out2 t) (out3 t))
   (init-stats)
   (let (*comment-table-1*
-        *comment-table-2*)
+        *comment-table-2*
+        *lex-errors-msgs-1*
+        *lex-errors-msgs-2*
+        *lexems-1*
+        *lexems-2*)
     (multiple-value-bind (res1 res2)
         (compare-two-str str1 str2)
       (when *comment-table-1*
+        (format t "Comment Table of str1:~%")
         (get-json-comments *comment-table-1* t))
       (when *comment-table-2*
         (get-json-comments *comment-table-2* t))
-      (get-json-res res1 out1)
-      (get-json-res res2 out2)
-      (get-stats-res out3))))
+      (when *lex-errors-msgs-1*
+        (format t "~%Lex-errors-msgs-1:~%")
+        (get-lexer-errors-msgs-json *lex-errors-msgs-1* t)
+        (format t "~%Lexems-1:~%")
+        (get-lexems-json *lexems-1* t))
+      (when *lex-errors-msgs-2*
+        (format t "~%Lex-errors-msgs-1:~%")
+        (get-lexer-errors-msgs-json *lex-errors-msgs-2* t)
+        (format t "~%Lexems-1:~%")
+        (get-lexems-json *lexems-2* t))
+      (when (and res1 res2)
+        (format t "~%Res1:~%")
+        (get-json-res res1 out1)
+        (format t "~%Res2:~%")
+        (get-json-res res2 out2)
+        (format t "~%Stats:~%")
+        (get-stats-res out3)))))
 
 (defun compare-two-files (filepath1 filepath2)
   (let ((str1 (read-file-into-string filepath1))
@@ -79,18 +125,26 @@
 (defun compare-two-str (str1 str2)
   (let ((ast-tree-1 (get-abstract-sem-tree-from-string str1 1))
         (ast-tree-2 (get-abstract-sem-tree-from-string str2 2)))
-    (compare-results)
+    (when (and ast-tree-1 ast-tree-2)
+      (compare-results))
     (values ast-tree-1 ast-tree-2)))
 
 (defun get-abstract-sem-tree-from-string (str cur-file)
   (multiple-value-bind (res-lexems comments lex-errors)
       (lexer str)
-    (when lex-errors
-      (error "Lex errors !!! ~A" lex-errors))
     (when (> (hash-table-count comments) 0)
       (cond ((= cur-file 1)
              (setf *comment-table-1* comments))
             ((= cur-file 2)
              (setf *comment-table-2* comments))
             (t (error "Error value of cur-file"))))
+    (when lex-errors
+        (cond ((= cur-file 1)
+               (setf *lex-errors-msgs-1* lex-errors)
+               (setf *lexems-1* res-lexems))
+              ((= cur-file 2)
+               (setf *lex-errors-msgs-2* lex-errors)
+               (setf *lexems-2* res-lexems))
+              (t (error "Error value of cur-file")))
+         (return-from get-abstract-sem-tree-from-string nil))
     (abstract-sem-tree-gen (parser res-lexems) :curr-file cur-file)))
